b032fa4cf450024d0d023203538314a449808b60
Gustavo de Oliveira <gust.oliveira.ferreira@gmail.com>
2023-12-13 01:28:19 -0300

    Improve harpoon commands

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -4,17 +4,14 @@ local harpoon = require("harpoon")
 harpoon:setup()
 -- REQUIRED
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>hr", function() harpoon:list():remove() end)
+vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
+vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
 
--- vim.keymap.set("n", "<C-H>", function() harpoon:list():removeAt(1) end)
--- vim.keymap.set("n", "<C-J>", function() harpoon:list():removeAt(2) end)
--- vim.keymap.set("n", "<C-K>", function() harpoon:list():removeAt(3) end)
--- vim.keymap.set("n", "<C-S>", function() harpoon:list():removeAt(4) end)
-
-
+vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>h;", function() harpoon:list():select(4) end)
