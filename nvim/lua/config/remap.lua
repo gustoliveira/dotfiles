3bedb2de722b84ae2c3c3b067b8174538f8fde86
Gustavo de Oliveira <gust.oliveira.ferreira@gmail.com>
2023-12-14 19:03:29 -0300

    Remap buffers moving keys

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,13 +1,10 @@
-vim.g.mapleader = " "					-- Set <leader> key to <space>
+vim.g.mapleader = " "				            	-- Set <leader> key to <space>
 
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)		-- Got to explore files
 
-vim.keymap.set("n", "ty", vim.cmd.bn)
-
+vim.keymap.set("n", "ty", vim.cmd.bn)               -- Go to next buffer
+vim.keymap.set("n", "tr", vim.cmd.bp)               -- Go to previous buffer
+vim.keymap.set("n", "td", vim.cmd.bd)               -- Delete buffer
 
 -- nmap te :tabe<CR>   " Create new tab
-
--- nmap ty :bn<CR>     " Go to left buffer
--- nmap tr :bp<CR>     " Go to right buffer
--- nmap td :bd<CR>     " Delete buffer
 
