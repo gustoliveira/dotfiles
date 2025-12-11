f3bd740a1065d1edf843147f8c71a2fe08f77618
Gustavo de Oliveira <gust.oliveira.ferreira@gmail.com>
2025-12-11 14:58:35 -0300

    Multiple themes and select them using telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,37 +1,3 @@
-local function normalize_path(path)
-  return path:gsub("\\", "/")
-end
-
-local function normalize_cwd()
-  return normalize_path(vim.loop.cwd()) .. "/"
-end
-
-local function is_subdirectory(cwd, path)
-  return string.lower(path:sub(1, #cwd)) == string.lower(cwd)
-end
-
-local function split_filepath(path)
-  local normalized_path = normalize_path(path)
-  local normalized_cwd = normalize_cwd()
-  local filename = normalized_path:match("[^/]+$")
-
-  if is_subdirectory(normalized_cwd, normalized_path) then
-    local stripped_path = normalized_path:sub(#normalized_cwd + 1, -(#filename + 1))
-    return stripped_path, filename
-  else
-    local stripped_path = normalized_path:sub(1, -(#filename + 1))
-    return stripped_path, filename
-  end
-end
-
-local function path_display(_, path)
-  local stripped_path, filename = split_filepath(path)
-  if filename == stripped_path or stripped_path == "" then
-    return filename
-  end
-  return string.format("%s -> %s", filename, stripped_path)
-end
-
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
@@ -53,6 +19,9 @@ return {
     vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
     vim.keymap.set('n', '<leader>fof', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { noremap = true, silent = true, desc = '[F]ind [R]eferences' })
+    vim.keymap.set('n', '<leader>ft', builtin.colorscheme, { noremap = true, silent = true, desc = '[F]ind [T]heme' })
+    vim.keymap.set('n', '<leader>fj', builtin.jumplist, { noremap = true, silent = true, desc = '[F]ind [J]jumplist' })
+    vim.keymap.set('n', '<leader>fb', builtin.builtin, { noremap = true, silent = true, desc = '[F]ind [B]uilt in commands' })
 
     vim.keymap.set('n', '<leader>/',
       function()
@@ -80,7 +49,6 @@ return {
       },
       defaults = {
         layout_strategy = "horizontal",
-        path_display = path_display,
         layout_config = {
           horizontal = {
             width = { padding = 10 },
--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,23 +1,37 @@
 return {
-  "tiagovla/tokyodark.nvim",
-  opts = {
-    transparent_background = false,
-    gamma = 1.00,
-    styles = {
-      comments = { italic = true },
-      keywords = { italic = true },
-      identifiers = { italic = true },
-      functions = {},
-      variables = {},
+  -- Dark Theme
+  {
+    "tiagovla/tokyodark.nvim",
+    opts = {
+      transparent_background = false,
+      gamma = 1.00,
+      styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        identifiers = { italic = true },
+        functions = {},
+        variables = {},
+      },
+      custom_highlights = function(_, _)
+        return {}
+      end,
+      custom_palette = {} or function(_) return {} end,
+      terminal_colors = true,
     },
-    custom_highlights = function(_, _)
-      return {}
+    config = function(_, opts)
+      require("tokyodark").setup(opts)
+      vim.cmd [[colorscheme tokyodark]]
     end,
-    custom_palette = {} or function(_) return {} end,
-    terminal_colors = true,
   },
-  config = function(_, opts)
-    require("tokyodark").setup(opts)
-    vim.cmd [[colorscheme tokyodark]]
-  end,
+  -- Ligh Theme
+  {
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('github-theme').setup({})
+      vim.cmd('colorscheme github_light')
+    end,
+  }
 }
