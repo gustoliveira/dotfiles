bd4d51b0344e4777c665a423c78178561b42c094
Gustavo de Oliveira <gust.oliveira.ferreira@gmail.com>
2026-02-22 21:35:09 -0300

    Lualine with new color palette

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,3 +1,46 @@
+local colors = {
+  blue   = '#0000FF',
+  green  = '#00CD00',
+  purple = '#660066',
+  cyan   = '#006666',
+  red1   = '#CD0000',
+  red2   = '#FF0000',
+  yellow = '#C4A000',
+  fg     = '#FFFFFF',
+  bg     = '#282C34',
+  gray1  = '#828997',
+  gray2  = '#2C323C',
+  gray3  = '#3E4452',
+}
+
+USGCTheme = {
+  normal = {
+    a = { fg = colors.bg, bg = colors.green },
+    b = { fg = colors.fg, bg = colors.gray3 },
+    c = { fg = colors.fg, bg = colors.gray2 },
+  },
+  command = {
+    a = { fg = colors.bg, bg = colors.yellow }
+  },
+  insert = {
+    a = { fg = colors.fg, bg = colors.blue, gui = 'bold' }
+  },
+  visual = {
+    a = { fg = colors.fg, bg = colors.purple }
+  },
+  terminal = {
+    a = { fg = colors.fg, bg = colors.cyan }
+  },
+  replace = {
+    a = { fg = colors.fg, bg = colors.red1 }
+  },
+  inactive = {
+    a = { fg = colors.gray1, bg = colors.bg },
+    b = { fg = colors.gray1, bg = colors.bg },
+    c = { fg = colors.gray1, bg = colors.gray2 },
+  },
+}
+
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -5,7 +48,8 @@ return {
     require("lualine").setup {
       options = {
         icons_enabled = true,
-        theme = 'onedark',
+        -- theme = 'onedark',
+        theme = USGCTheme,
         component_separators = { left = '', right = '' },
         section_separators = {},
         disabled_filetypes = {
@@ -69,3 +113,5 @@ return {
     }
   end
 }
+
+
